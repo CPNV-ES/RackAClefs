f5f4872731ec1af4f18c8af658a04bc5f9b2ba80
Jonathan DEBETAZ <redkill2108@acegames.fr>
2017-03-10 12:17:41 +0100

    Update doc

--- docs/gen-doc.js
+++ docs/gen-doc.js
@@ -1,6 +1,6 @@
 const markdownpdf = require("markdown-pdf")
 const fs = require('fs')
 
-fs.createReadStream(__dirname + "/Dossier de projet - RackAClés.md")
+fs.createReadStream(__dirname + "/Dossier de projet - RackAClefs.md")
   .pipe(markdownpdf())
-  .pipe(fs.createWriteStream(__dirname + "/Dossier de projet - RackAClés.pdf"))
+  .pipe(fs.createWriteStream(__dirname + "/Dossier de projet - RackAClefs.pdf"))
