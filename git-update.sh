846c655ad9d9c3e07d31c0899b3c215320112def
Jonathan DEBETAZ <redkill2108@acegames.fr>
2017-03-10 12:05:02 +0100

    Update Git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -3,5 +3,5 @@ git config --global user.name "Redkill2108"
 git add -f . > /dev/null
 git remote add update https://redkill:$GITHUB_API_KEY@github.com/CPNV-ES/RackAClefs.git
 git commit -m "Travis CI build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
-git push -fq update master
+git push -f update master
 echo -e "Done\n"
